90a1f0a29423788b2992f060d0dc5d51a1f60372
Andymota029 <129234074+Nino029@users.noreply.github.com>
2024-09-05 19:26:40 -0400

    Add files via upload

---
+++ Ejercicio python.py
@@ -0,0 +1,9 @@
+
+edad = int(input("Ingrese la edad de la persona: "))
+
+if edad >= 60:
+    print("Esta persona pertenece a la tercera edad")
+elif edad >= 18:
+    print("Esta persona es mayor de edad")
+else:
+    print("Esta persona es menor de edad")
